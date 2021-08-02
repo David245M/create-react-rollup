00bf63d2b06dccac5890a10539d3e13b95a047ba
Michael Kokadii <misha2479k@gmail.com>
2021-08-02 12:59:06 +0300

    Added more readable CLI

--- start.js
+++ start.js
@@ -1,6 +1,6 @@
 const fs = require('fs-extra')
-const path = require('path')
 const util = require('util')
+const Listr = require('listr')
 const { projectInstall } = require('pkg-install')
 const exec = util.promisify(require('child_process').exec)
 
@@ -15,58 +15,66 @@ export async function createProject(args) {
   const [, rawFrom, directory] = args
   const from = rawFrom.split('\\').slice(0,-2).join('\\')
 
-  if (!directory) {
-    console.error('Please provide directory name and try again.')
-    return
-  }
+  const tasks = new Listr([
+    {
+      title: 'Creating new directory',
+      task: async () => {
+        if (!directory) {
+          return Error('Please provide directory name and try again.')
+        }
 
-  try {
-    if (fs.existsSync(directory)) {
-      console.error(`Directory ${directory} is alredy exists! Remove it or choose another name for app and try again.`)
-      return
-    }
-    await fs.mkdir(directory)
-    console.log(`Directory ${directory} has created.`)
-
-    // creating subdirectories...
-    await fs.mkdir(`${directory}/public`)
-    await fs.mkdir(`${directory}/src`)
-
-    // and copy files into them
-    await fs.copy(`${from}/public`,`${directory}/public`)
-    await fs.copy(`${from}/src`, `${directory}/src`)
-    await fs.copyFile(`${from}/rollup.config.js`, `${directory}/rollup.config.js`)
-    console.log('Files copied.')
-    
-    // getting current (dev-) dependencies
-    const devPack = await fs.readFile(`${from}/package.json`)
-    let { dependencies, devDependencies } = JSON.parse(devPack)
-    dependencies = {
-      'react': dependencies['react'],
-      'react-dom': dependencies['react-dom'],
-    }
-
-    // initialization project
-    await exec(`npm init -y`, {
-      cwd: directory,
-    })
-    const pack = await fs.readFile(`${directory}/package.json`)
-    const packJSON = JSON.parse(pack.toString())
-    const packWithScripts = JSON.stringify({
-      ...packJSON,
-      ...npmOptions,
-      dependencies,
-      devDependencies,
-    }, null, '\t')
-    await fs.writeFile(`${directory}/package.json`, packWithScripts)
-    console.log('Copy dependencies.')
-    
-    // installing dependencies 
-    const x = await projectInstall({
-      cwd: directory,
-    })
-  } catch (err) {
-    console.error(err)
-  }
-}
+        if (fs.existsSync(directory)) {
+          return Error(`Directory ${directory} is alredy exists! Remove it or choose another name for app and try again.`)
+        }
+        await fs.mkdir(directory)
+        return `Directory ${directory} has created.`
+      }
+    },
+    {
+      title: 'Copying files to new directory',
+      task: async () => {
+        // creating subdirectories...
+        await fs.mkdir(`${directory}/public`)
+        await fs.mkdir(`${directory}/src`)
   
+        // and copy files into them
+        await fs.copy(`${from}/public`,`${directory}/public`)
+        await fs.copy(`${from}/src`, `${directory}/src`)
+        await fs.copyFile(`${from}/rollup.config.js`, `${directory}/rollup.config.js`)
+        return 'Copied!'
+      }
+    },
+    {
+      title: 'Project initialization',
+      task: async () => {
+        // getting current (dev-) dependencies
+        const devPack = await fs.readFile(`${from}/package.json`)
+        let { dependencies, devDependencies } = JSON.parse(devPack)
+        dependencies = {
+          'react': dependencies['react'],
+          'react-dom': dependencies['react-dom'],
+        }
+
+        await exec(`npm init -y`, {
+          cwd: directory,
+        })
+
+        const pack = await fs.readFile(`${directory}/package.json`)
+        const packJSON = JSON.parse(pack.toString())
+        const packWithScripts = JSON.stringify({
+          ...packJSON,
+          ...npmOptions,
+          dependencies,
+          devDependencies,
+        }, null, '\t')
+        await fs.writeFile(`${directory}/package.json`, packWithScripts)
+      }
+    },
+    {
+      title: 'Installing dependencies',
+      task: async () => await projectInstall({ cwd: directory })
+    }
+  ])
+
+  return await tasks.run()
+}
